440638d176c83010d52f5c1021ddb59061d8faaf
“Brandon <balvarez325@gmail.com>
2022-10-27 10:38:16 -0400

    Update component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Nav from "./components/nav/Nav";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      {/* <header className="App-header">Hello World</header> */}
+      <Nav />
+      <Sidebar />
+      <Main />
+      <Footer />
     </div>
   );
 }
