2495ba87396c6cf44dedeabc2221d2d85d2071cb
“Brandon <balvarez325@gmail.com>
2022-10-27 16:25:53 -0400

    Update CSS

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,12 +7,12 @@ function Main() {
       <div className="top-6-container">
         <p className="greeting">Good Morning</p>
         <div className="top-6">
-          <TP title={"Playlist 1"} />
-          <TP title={"Playlist 2"} />
-          <TP title={"Playlist 3"} />
-          <TP title={"Playlist 4"} />
-          <TP title={"Playlist 5"} />
-          <TP title={"Playlist 6"} />
+        <TP title={"Playlist 1"} />
+        <TP title={"Playlist 2"} />
+        <TP title={"Playlist 3"} />
+        <TP title={"Playlist 4"} />
+        <TP title={"Playlist 5"} />
+        <TP title={"Playlist 6"} />
         </div>
       </div>
     </div>
