83e6fcf5c338b85770f52e425468557b4b17b82d
“Brandon <balvarez325@gmail.com>
2022-10-27 15:06:36 -0400

    update imports and CSS

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,21 @@
 import React from "react";
+import TP from "./Top-Playlist.jsx";
 
 function Main() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div className="top-6-container">
+        <div className="top-6">
+          <TP />
+          <TP />
+          <TP />
+          <TP />
+          <TP />
+          <TP />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
---
+++ src/components/main/Top-Playlist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TopPlaylist() {
+  return <div>Top-Playlist</div>;
+}
+
+export default TopPlaylist;
