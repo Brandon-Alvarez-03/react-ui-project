1331bc38acb2c7b85bc2721044cc41727d3eea04
“Brandon <balvarez325@gmail.com>
2022-10-28 11:55:48 -0400

    css looking fresh

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,8 +6,22 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="top-links">
-        <Links />
-        <Links />
+        <Links
+          icon1={"Home"}
+          icon2={"Search"}
+          icon3={"Menu_Open"}
+          description1={"Home"}
+          description2={"Search"}
+          description3={"Your Library"}
+        />
+        <Links
+          icon1={"Add_Box"}
+          icon2={"Favorite"}
+          icon3={"Bookmark"}
+          description1={"Add Playlist"}
+          description2={"Liked Songs"}
+          description3={"Your Episodes"}
+        />
       </div>
       <div className="bottom-links">
         <Playlists />
